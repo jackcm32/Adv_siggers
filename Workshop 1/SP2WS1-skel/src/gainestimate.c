2862e8f2d4c7b8626cf65f38ad7cb203bbd4bced
Jack Hurley <jack.hurley23@gmail.com>
2017-04-10 16:45:02 +1000

    RLS

--- Workshop 1/SP2WS1-skel/src/gainestimate.c
+++ Workshop 1/SP2WS1-skel/src/gainestimate.c
@@ -50,4 +50,39 @@ void gainestimateLMS(float in1, float in2, float out, float gain[2])
 void gainestimateRLS(float in1, float in2, float out, float gain[2])
 {
     // TODO: Implement gain estimation using RLS algorithm
+    static float theta_hat1 = 0, theta_hat2 = 1.0, p11 = 0, p12 = 0, p21 = 0, p22 = 0;
+
+    float err = out - (in1 * theta_hat1 + in2 * theta_hat2);
+
+
+    float p11_in1 = p11 * in1;
+    float p11_in2 = p11 * in2;
+    float p12_in1 = p12 * in1;
+    float p12_in2 = p12 * in1;
+    float p21_in1 = p21 * in1;
+    float p21_in2 = p21 * in2;
+    float p22_in1 = p22 * in1;
+    float p22_in2 = p22 * in2;
+
+    float inv_lambda = 1 / lambda;
+
+    float denom = (lambda + in1 * (p11_in1 + p21_in2) + in2 * (p12_in1 + p22_in2));
+
+    float p11_new = inv_lambda * (p11 - (p11_in1 * (p11_in1 + p12_in2) + p21_in2 * (p11_in1 + p12*in2)) / denom);
+    float p12_new = inv_lambda * (p12 - (p12_in1 * (p11_in1 + p12_in2) + p22_in2 * (p11_in1 + p12*in2)) / denom);
+    float p21_new = inv_lambda * (p21 - (p11_in1 * (p21_in1 + p22_in2) + p21_in2 * (p21_in1 + p22*in2)) / denom);
+    float p22_new = inv_lambda * (p22 - (p12_in1 * (p21_in1 + p22_in2) + p22_in2 * (p21_in1 + p22*in2)) / denom);
+
+
+    p11 = p11_new;
+    p12 = p12_new;
+    p21 = p21_new;
+    p22 = p22_new;
+
+    theta_hat1 = theta_hat1 + ((p11 * in1) + (p12 * in2)) * err;
+    theta_hat2 = theta_hat2 + ((p21 * in1) + (p22 * in2)) * err;
+
+
+    // then save p values for next round
+
 }
