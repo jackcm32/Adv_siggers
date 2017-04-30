0765d679a8161de37420c15a9a972857c4502a29
Jack Mackintosh <jack.c.mackintosh@gmail.com>
2017-04-30 20:04:02 +1000

    Update the time varying (1B stuff) Added RLS2 function for RLS with forgetting factor

---
+++ Workshop 1/RLS_function2.m
@@ -0,0 +1,43 @@
+function [ output, theta_hat ] = RLS_function2(loudspeaker, mike1, init_params, P_init_scale, lambda)
+
+Fs = 8192; %sampling freq
+D1 = 1 * Fs; % delay 1
+D2 = 2.5 * Fs; % delay 2
+
+P = P_init_scale * eye(3);
+
+theta_hat = zeros(3,length(loudspeaker));
+loudspeakerDelay1 = 0;
+loudspeakerDelay2 = 0;
+
+output  = zeros(1,length(loudspeaker));
+
+
+for i=1:length(loudspeaker)
+    
+    if (i > D1)
+        loudspeakerDelay1 = loudspeaker(i - D1);
+    end
+       
+    if (i > D2)
+        loudspeakerDelay2 = loudspeaker(i - D2);
+    end 
+    
+    phi = [loudspeaker(i); loudspeakerDelay1;  loudspeakerDelay2];
+    
+    P = (1/lambda)*(P - (P*phi*transpose(phi)*P)/(lambda + transpose(phi)*P*phi));
+
+    G = P*phi;
+    
+    if ( i > 1)
+        theta_hat(:,i) = theta_hat(:,i-1) + G*(mike1(i) - transpose(phi)*theta_hat(:,i-1));
+    else
+        theta_hat(:,i) = init_params + G*(mike1(i) - transpose(phi)*init_params);
+    end
+    
+    output(i) = mike1(i) - theta_hat(2,i)*loudspeakerDelay1 - theta_hat(3,i)*loudspeakerDelay2;
+end
+
+
+end
+
--- Workshop 1/Workshop1B.m
+++ Workshop 1/Workshop1B.m
@@ -1,7 +1,8 @@
 %% B.1
-% Time varying RLS without noise
+% RLS without noise
 
 clear all
+close all
 clc
 load('data1.mat');
 load('data2.mat');
@@ -9,212 +10,194 @@ load('data2.mat');
 Fs = 8192; %sampling freq
 D1 = 1 * Fs; % delay 1
 D2 = 2.5 * Fs; % delay 2
+timespan = [0: 1/Fs: length(loudspeaker)/Fs - 1/Fs]; % Timespan for time-axis
 
-loudspeakerDelay1 = 0;
-loudspeakerDelay2 = 0;
 
-a1 = 0;
-a2 = 0;
-a3 = 0;
+% RLS with inital params = 0
 
-theta_hat = [a1; a2; a3];
-
+init_params = [1; 0; 0];
+P_init_scale = 1;
 lambda = 0.995;
 
-P = eye(3);
 
-output  = zeros(1,length(loudspeaker));
+[output, theta_hat] = RLS_function2(loudspeaker, mike2, init_params, P_init_scale, lambda);
 
-for i=1:length(loudspeaker)
-    if (i > D1)
-        loudspeakerDelay1 = loudspeaker(i - D1);
-    end
-       
-    if (i > D2)
-        loudspeakerDelay2 = loudspeaker(i - D2);
-    end 
-    
-    phi = [loudspeaker(i); loudspeakerDelay1;  loudspeakerDelay2];
-    
-    P = (1/lambda)*(P - (P*phi*transpose(phi)*P)/(lambda + transpose(phi)*P*phi));
-    
-    G = P*phi;
+figure(1)
+hold on
+plot(timespan,theta_hat(1,:))
+plot(timespan,theta_hat(2,:))
+plot(timespan, theta_hat(3,:))
+hold off
 
-    theta_hat = theta_hat + G*(mike2(i) - transpose(phi)*theta_hat);
-    
-    output(i) = mike2(i) - theta_hat(2)*loudspeakerDelay1 - theta_hat(3)*loudspeakerDelay2;
-    
-end
+title('Time varying Parameter values (RLS no noise)')
+xlabel('Seconds')
+ylabel('Parameter value')
+legend('b_1', 'b_2', 'b_3')
+ylim([0 1])
+
+saveas(gcf, 'figures/q1b_params.png')
+
+%%
+% RLS lambda changing
+clear all
+close all
+clc
+load('data1.mat');
+load('data2.mat')
+
+Fs = 8192; %sampling freq
+D1 = 1 * Fs; % delay 1
+D2 = 2.5 * Fs; % delay 2
+timespan = [0: 1/Fs: length(loudspeaker)/Fs - 1/Fs]; % Timespan for time-axis
 
 
-% sound(loudspeaker)
-% pause
-% sound(noisymike2)
-% pause
-sound(output)
+% RLS with P_init_scale = 1
+init_params = [1; 0; 0];
+P_init_scale = 1;
+lambda = 0.999;
+
+[output1, theta_hat] = RLS_function2(loudspeaker, mike2, init_params, P_init_scale, lambda);
+
+figure(2)
+subplot(3,1,1)
+hold on
+plot(timespan,theta_hat(1,:))
+plot(timespan,theta_hat(2,:))
+plot(timespan, theta_hat(3,:))
+hold off
+
+title('Time varying Parameter values (RLS no noise) Lambda = 0.999')
+xlabel('Seconds')
+ylabel('Parameter value') 
+legend('b_1', 'b_2', 'b_3')
+ylim([0 1.01])
+
+
+% RLS with lambda = 0.990;
+lambda = 0.990;
+
+[output001, theta_hat] = RLS_function2(loudspeaker, mike2, init_params, P_init_scale, lambda);
+
+figure(2)
+subplot(3,1,2)
+hold on
+plot(timespan,theta_hat(1,:))
+plot(timespan,theta_hat(2,:))
+plot(timespan, theta_hat(3,:))
+hold off
+
+title('Time varying Parameter values (RLS no noise) lambda = 0.990;')
+xlabel('Seconds')
+ylabel('Parameter value')
+legend('b_1', 'b_2', 'b_3')
+ylim([0 1.01])
+
+% RLS with lambda = 0.999;
+init_params = [1; 0; 0];
+P_init_scale = 100;
+lambda = 0.995;
+
+[output100, theta_hat] = RLS_function2(loudspeaker, mike2, init_params, P_init_scale, lambda);
+
+figure(2)
+subplot(3,1,3)
+hold on
+plot(timespan,theta_hat(1,:))
+plot(timespan,theta_hat(2,:))
+plot(timespan, theta_hat(3,:))
+hold off
+
+title('Time varying Parameter values (RLS no noise) lambda = 0.995')
+xlabel('Seconds')
+ylabel('Parameter value')
+legend('b_1', 'b_2', 'b_3')
+ylim([0 1.01])
+
+saveas(gcf, 'figures/q1b_Pinit.png')
+
+figure(6)
+subplot(3,1,1)
+plot(timespan, transpose(loudspeaker) - output1)
+title('Amplitude Difference for Time Varying Echo Amplitude (RLS without noise) lambda = 0.999;')
+xlabel('Seconds')
+ylabel('Amplitude')
+
+subplot(3,1,2)
+plot(timespan, transpose(loudspeaker) - output001)
+title('Amplitude Difference for Time Varying Echo Amplitudes(RLS without noise) lambda = 0.990;')
+xlabel('Seconds')
+ylabel('Amplitude')
+
+subplot(3,1,3)
+plot(timespan, transpose(loudspeaker) - output100)
+title('Amplitude Difference for Time Varying Echo Amplitudes (RLS without noise) lambda = 0.995;')
+xlabel('Seconds')
+ylabel('Amplitude')
+
+saveas(gcf, 'figures/q1b_Pinit_output_diff.png')
 
 %% B.1
-% Time varying RLS with noise
+% RLS with noise
 
 clear all
+close all
 clc
 load('data1.mat');
-load('data2.mat');
+load('data2.mat')
 
 Fs = 8192; %sampling freq
 D1 = 1 * Fs; % delay 1
 D2 = 2.5 * Fs; % delay 2
+timespan = [0: 1/Fs: length(loudspeaker)/Fs - 1/Fs]; % Timespan for time-axis
 
-loudspeakerDelay1 = 0;
-loudspeakerDelay2 = 0;
 
-a1 = 0;
-a2 = 0;
-a3 = 0;
-
-theta_hat = [a1; a2; a3];
-
+init_params = [1; 0; 0];
+P_init_scale = 1;
 lambda = 0.995;
 
-P = eye(3);
+[output, theta_hat] = RLS_function2(loudspeaker, mike2, init_params, P_init_scale, lambda);
+[noisy_output, noisy_theta_hat] = RLS_function2(loudspeaker, noisymike2, init_params, P_init_scale, lambda);
 
-noisy_output  = zeros(1,length(loudspeaker));
-
-for i=1:length(loudspeaker)
-    if (i > D1)
-        loudspeakerDelay1 = loudspeaker(i - D1);
-    end
-       
-    if (i > D2)
-        loudspeakerDelay2 = loudspeaker(i - D2);
-    end 
-    
-    phi = [loudspeaker(i); loudspeakerDelay1;  loudspeakerDelay2];
-    
-    P = (1/lambda)*(P - (P*phi*transpose(phi)*P)/(lambda + transpose(phi)*P*phi));
-    
-    G = P*phi;
-
-    theta_hat = theta_hat + G*(noisymike2(i) - transpose(phi)*theta_hat);
-    
-    noisy_output(i) = noisymike2(i) - theta_hat(2)*loudspeakerDelay1 - theta_hat(3)*loudspeakerDelay2; 
-    
-end
-
-% sound(loudspeaker)
-% pause
-% sound(noisymike2)
-% pause
-sound(noisy_output)
-
-%% B.2 
-% Time varying LMS without noise
-
-clear all
-clc
-load('data1.mat');
-load('data2.mat');
-
-Fs = 8192; %sampling freq
-D1 = 1 * Fs; % delay 1
-D2 = 2.5 * Fs; % delay 2
-
-loudspeakerDelay1 = 0;
-loudspeakerDelay2 = 0;
-
-a1 = 0;
-a2 = 0;
-a3 = 0;
-
-theta_hat = [a1; a2; a3];
-
-P = eye(3);
-
-output  = zeros(1,length(loudspeaker));
-
-% Estimating mu
-upper_mu = 1/mean(mike2.^2);
-disp(['Upper mu = ' num2str(upper_mu)])
-mu = 3;
+% sound(noisy_output)
 
 
-for i=1:length(loudspeaker)
-    if (i > D1)
-        loudspeakerDelay1 = loudspeaker(i - D1);
-    end
-       
-    if (i > D2)
-        loudspeakerDelay2 = loudspeaker(i - D2);
-    end 
-    
-    phi = [loudspeaker(i); loudspeakerDelay1;  loudspeakerDelay2];
-    
-    G = mu*phi;
+figure(4)
+subplot(2,1,1)
+hold on
+plot(timespan,theta_hat(1,:))
+plot(timespan,theta_hat(2,:))
+plot(timespan, theta_hat(3,:))
+hold off
 
-    theta_hat = theta_hat + G*(mike2(i) - transpose(phi)*theta_hat);
-    
-    output(i) = mike2(i) - theta_hat(2)*loudspeakerDelay1 - theta_hat(3)*loudspeakerDelay2; 
-end
+title('Time Varying Parameter values (RLS no noise)')
+xlabel('Seconds')
+ylabel('Parameter value')
+legend('b_1', 'b_2', 'b_3')
+ylim([0 1])
 
+subplot(2,1,2)
+hold on
+plot(timespan,noisy_theta_hat(1,:))
+plot(timespan,noisy_theta_hat(2,:))
+plot(timespan, noisy_theta_hat(3,:))
+hold off
 
-% sound(loudspeaker)
-% pause
-% sound(mike2)
-% pause
-sound(output)
+title('Time varying Parameter values (RLS with noise)')
+xlabel('Seconds')
+ylabel('Parameter value')
+legend('b_1', 'b_2', 'b_3')
+ylim([0 1])
 
-%% A.2 
-% Time varying LMS with noise
-
-clear all
-clc
-load('data1.mat');
-load('data2.mat');
-
-Fs = 8192; %sampling freq
-D1 = 1 * Fs; % delay 1
-D2 = 2.5 * Fs; % delay 2
-
-loudspeakerDelay1 = 0;
-loudspeakerDelay2 = 0;
-
-a1 = 0;
-a2 = 0;
-a3 = 0;
-
-theta_hat = [a1; a2; a3];
-
-P = eye(3);
-
-noisy_output  = zeros(1,length(loudspeaker));
-
-% Estimating mu
-upper_mu = 1/mean(noisymike2.^2);
-disp(['Upper mu = ' num2str(upper_mu)])
-mu = 3;
-
-for i=1:length(loudspeaker)
-    if (i > D1)
-        loudspeakerDelay1 = loudspeaker(i - D1);
-    end
-       
-    if (i > D2)
-        loudspeakerDelay2 = loudspeaker(i - D2);
-    end 
-    
-    phi = [loudspeaker(i); loudspeakerDelay1;  loudspeakerDelay2];
-    
-    G = mu*phi;
-
-    theta_hat = theta_hat + G*(noisymike2(i) - transpose(phi)*theta_hat);
-   
-    noisy_output(i) = noisymike2(i) - theta_hat(2)*loudspeakerDelay1 - theta_hat(3)*loudspeakerDelay2; 
-end
-
-% 
-% sound(loudspeaker)
-% pause
-% sound(noisymike2)
-% pause
-sound(noisy_output)
-
+figure(5)
+subplot(2,1,1)
+plot(timespan, transpose(loudspeaker) - output)
+title('Amplitude Difference between Loudspeaker and output signal for Time Varying Echo Amplitudes(RLS without noise)')
+xlabel('Seconds')
+ylabel('Amplitude')
+ylim([-0.1 0.15])
+subplot(2,1,2)
+plot(timespan, transpose(loudspeaker) - noisy_output)
+title('Amplitude Difference between Loudspeaker and output signalfor Time Varying Echo Amplitudes(RLS with noise)')
+xlabel('Seconds')
+ylabel('Amplitude')
+ylim([-0.1 0.15])
