4d285d34949053d9a278ca4712be7d829ebfea3a
Jack Hurley <jack.hurley23@gmail.com>
2017-04-30 20:00:17 +1000

    Implemented LMS algorithm

--- Workshop 1/Workshop1A.m
+++ Workshop 1/Workshop1A.m
@@ -168,7 +168,7 @@ D2 = 2.5 * Fs; % delay 2
 timespan = [0: 1/Fs: length(loudspeaker)/Fs - 1/Fs]; % Timespan for time-axis
 
 
-init_params = [0; 0; 0];
+init_params = [1; 0; 0];
 P_init_scale = 1;
 
 [output, theta_hat] = RLS_function(loudspeaker, mike1, init_params, P_init_scale);
@@ -204,6 +204,8 @@ ylabel('Parameter value')
 legend('b_1', 'b_2', 'b_3')
 ylim([0 1])
 
+saveas(gcf, 'figures/q1a_noise_params.png')
+
 figure(5)
 subplot(2,1,1)
 plot(timespan, transpose(loudspeaker) - output)
@@ -218,6 +220,8 @@ xlabel('Seconds')
 ylabel('Amplitude')
 ylim([-0.1 0.15])
 
+saveas(gcf, 'figures/q1a_noise_diff.png')
+
 
 
 %% A.2 
@@ -225,85 +229,185 @@ ylim([-0.1 0.15])
 
 clear all
 clc
+close all
 load('data1.mat');
 
 Fs = 8192; %sampling freq
 D1 = 1 * Fs; % delay 1
 D2 = 2.5 * Fs; % delay 2
+timespan = [0: 1/Fs: length(loudspeaker)/Fs - 1/Fs]; % Timespan for time-axis
 
-
-% LMS no noise with mu = 3
+% LMS no noise with mu = 3 b2=b3 = 0
 init_params = [1; 0; 0];
-mu = 3;
-
+mu = 0.3;
 [output, theta_hat] = LMS_function(loudspeaker, mike1, init_params, mu);
 
+% LMS no noise with mu = 3 b2=b3 = 1
+init_params = [1; 1; 1];
+[output2, theta_hat2] = LMS_function(loudspeaker, mike1, init_params, mu);
+
+
 figure(2)
-subplot(3,1,2)
+subplot(2,1,1)
 hold on
 plot(timespan,theta_hat(1,:))
 plot(timespan,theta_hat(2,:))
 plot(timespan, theta_hat(3,:))
 hold off
 
-title('Parameter values (LMS no noise) mu = 0.01')
+title('Parameter values (LMS no noise) mu = 0.3; b_2 = b_3 = 1')
 xlabel('Seconds')
 ylabel('Parameter value')
 legend('b_1', 'b_2', 'b_3')
 ylim([0 1.01])
+
+subplot(2,1,2)
+hold on
+plot(timespan,theta_hat2(1,:))
+plot(timespan,theta_hat2(2,:))
+plot(timespan, theta_hat2(3,:))
+hold off
+
+title('Parameter values (LMS no noise) mu = 0.3; b_2 = b_3 = 1')
+xlabel('Seconds')
+ylabel('Parameter value')
+legend('b_1', 'b_2', 'b_3')
+ylim([0 1.01])
+
+saveas(gcf,'figures/q1a_lms_params.png')
+
+
+%%
+% LMS mu graphs
+
+
+clear all
+clc
+close all
+load('data1.mat');
+
+Fs = 8192; %sampling freq
+D1 = 1 * Fs; % delay 1
+D2 = 2.5 * Fs; % delay 2
+timespan = [0: 1/Fs: length(loudspeaker)/Fs - 1/Fs]; % Timespan for time-axis
+
+% LMS no noise with mu = 3 b2=b3 = 0
+init_params = [1; 0; 0];
+mu = 0.3;
+[output, theta_hat] = LMS_function(loudspeaker, mike1, init_params, mu);
+
+% LMS no noise with mu = 0.03 b2=b3 = 0
+mu = 0.03;
+[output2, theta_hat2] = LMS_function(loudspeaker, mike1, init_params, mu);
+
+% LMS no noise with mu = 3 b2=b3 = 0
+mu = 3;
+[output3, theta_hat3] = LMS_function(loudspeaker, mike1, init_params, mu);
+
+
+figure(2)
+subplot(3,1,1)
+hold on
+plot(timespan,theta_hat(1,:))
+plot(timespan,theta_hat(2,:))
+plot(timespan, theta_hat(3,:))
+hold off
+
+title('Parameter values (LMS no noise) mu = 0.3')
+xlabel('Seconds')
+ylabel('Parameter value')
+legend('b_1', 'b_2', 'b_3')
+ylim([0 1.01])
+
+subplot(3,1,2)
+hold on
+plot(timespan,theta_hat2(1,:))
+plot(timespan,theta_hat2(2,:))
+plot(timespan, theta_hat2(3,:))
+hold off
+
+title('Parameter values (LMS no noise) mu = 0.03')
+xlabel('Seconds')
+ylabel('Parameter value')
+legend('b_1', 'b_2', 'b_3')
+ylim([0 1.01])
+
+subplot(3,1,3)
+hold on
+plot(timespan,theta_hat3(1,:))
+plot(timespan,theta_hat3(2,:))
+plot(timespan, theta_hat3(3,:))
+hold off
+
+title('Parameter values (LMS no noise) mu = 3')
+xlabel('Seconds')
+ylabel('Parameter value')
+legend('b_1', 'b_2', 'b_3')
+ylim([0 1.01])
+
+saveas(gcf,'figures/q1a_lms_mu.png')
 
 
 %% A.2 
 % LMS with noise
 
 clear all
+close all
 clc
 load('data1.mat');
 
 Fs = 8192; %sampling freq
 D1 = 1 * Fs; % delay 1
 D2 = 2.5 * Fs; % delay 2
+timespan = [0: 1/Fs: length(loudspeaker)/Fs - 1/Fs]; % Timespan for time-axis
 
-loudspeakerDelay1 = 0;
-loudspeakerDelay2 = 0;
+init_params = [1; 0; 0];
+mu = 0.3;
 
-a1 = 0;
-a2 = 0;
-a3 = 0;
+[output, theta_hat] = LMS_function(loudspeaker, mike1, init_params, mu);
+[noisy_output, noisy_theta_hat] = LMS_function(loudspeaker, noisymike1, init_params, mu);
 
-theta_hat = [a1; a2; a3];
+figure(4)
+subplot(2,1,1)
+hold on
+plot(timespan,theta_hat(1,:))
+plot(timespan,theta_hat(2,:))
+plot(timespan, theta_hat(3,:))
+hold off
 
-P = eye(3);
+title('Parameter values (LMS no noise)')
+xlabel('Seconds')
+ylabel('Parameter value')
+legend('b_1', 'b_2', 'b_3')
+ylim([0 1])
 
-% Estimating mu
-upper_mu = 1/mean(noisymike1.^2);
-disp(['Upper mu = ' num2str(upper_mu)])
-mu = 3;
+subplot(2,1,2)
+hold on
+plot(timespan,noisy_theta_hat(1,:))
+plot(timespan,noisy_theta_hat(2,:))
+plot(timespan, noisy_theta_hat(3,:))
+hold off
 
-noisy_output  = zeros(1,length(loudspeaker));
+title('Parameter values (LMS with noise)')
+xlabel('Seconds')
+ylabel('Parameter value')
+legend('b_1', 'b_2', 'b_3')
+ylim([0 1])
 
-for i=1:length(loudspeaker)
-    if (i > D1)
-        loudspeakerDelay1 = loudspeaker(i - D1);
-    end
-       
-    if (i > D2)
-        loudspeakerDelay2 = loudspeaker(i - D2);
-    end 
-    
-    phi = [loudspeaker(i); loudspeakerDelay1;  loudspeakerDelay2];
-    
-    G = mu*phi;
+saveas(gcf, 'figures/q1a_lms_noise_params.png')
 
-    theta_hat = theta_hat + G*(noisymike1(i) - transpose(phi)*theta_hat);
-   
-    noisy_output(i) = noisymike1(i) - theta_hat(2)*loudspeakerDelay1 - theta_hat(3)*loudspeakerDelay2; 
-    
-end
+figure(5)
+subplot(2,1,1)
+plot(timespan, transpose(loudspeaker) - output)
+title('Amplitude Difference between Loudspeaker and output signal(LMS without noise)')
+xlabel('Seconds')
+ylabel('Amplitude')
+ylim([-0.1 0.15])
+subplot(2,1,2)
+plot(timespan, transpose(loudspeaker) - noisy_output)
+title('Amplitude Difference between Loudspeaker and output signal(LMS with noise)')
+xlabel('Seconds')
+ylabel('Amplitude')
+ylim([-0.1 0.15])
 
-% sound(loudspeaker)
-% pause
-% sound(noisymike1)
-% pause
-% sound(noisy_output)
-
+saveas(gcf, 'figures/q1a_lms_noise_diff.png')
