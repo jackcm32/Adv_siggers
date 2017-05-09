bf8e94e36d2746b8c6c5c57650573157f82b1326
Jack Hurley <jack.hurley23@gmail.com>
2017-05-09 15:17:06 +1000

    Update to Simulate System.  Derives true model with noise.

--- Workshop 2/simulate_system.m
+++ Workshop 2/simulate_system.m
@@ -1,12 +1,30 @@
-function [ ] = simulate_system( F, U, H, W, initial, input, T_FINAL )
-%SIMULATE_SYSTEM Summary of this function goes here
-%   Detailed explanation goes here
+function [ ] = simulate_system( F, U, H, W, h_init, input, mu, sigma_d, sigma_w, T_FINAL )
+% System Simulator
 
-    sys = ss(F,U,H,W,[]);
+    h3_output = zeros(1,T_FINAL);
+    
+    length(h3_output)
+    
+    
+    h = h_init;
 
-    T = [1:T_FINAL];
+    for t = 1:T_FINAL 
 
-    figure()
-    lsim(sys,input,T,initial);
+    %   Calculate Noise
+        d2 = normrnd(mu, sigma_d);
+        d3 = normrnd(mu, sigma_d);
+        V = [0; d2; d3];
+        w3 = normrnd(mu, sigma_w);
+
+    %   Update states  
+    % SUS THIS.......
+        h3_output(t) = H*h + W*w3;
+        h = F*h + U*input(t) + V;
+    end
+    
+    figure();
+    plot(1:T_FINAL,h3_output, 'r')
+    xlabel('Time (Minutes)')
+    ylabel('z_3 water level')
 
 end
